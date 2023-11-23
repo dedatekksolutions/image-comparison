22a4baa574a340cb8cff415aa1f47244d586a85e
s3632442 <s363244@student.rmit.edu.au>
2023-11-23 22:38:05 +1100

    not working at all

--- lambda_function.py
+++ lambda_function.py
@@ -1,51 +1,87 @@
-import boto3
+
 import hashlib
+import boto3
+from PIL import Image
+import imagehash
 import json
 
 def lambda_handler(event, context):
-    # Update bucket names and paths
-    approved_bucket = 'retroideal-member-vehicle-images'
-    approved_prefix = 'approved-vehicle-images/'
-
-    pending_bucket = 'retroideal-member-vehicle-images'
-    pending_prefix = 'pending-vehicle-images/'
-
-    # Update DynamoDB table name
-    table_name = 'retroideal-vehicle-image-table'
-
-    # Create S3 and DynamoDB clients
-    s3 = boto3.client('s3')
-    dynamodb = boto3.client('dynamodb')
-
-    # Check if the event is from API Gateway
+    # Extract payload data
     if 'body' in event:
         payload = json.loads(event['body'])
     else:
-        # For local testing
-        payload = event
+        payload = event.get('queryStringParameters', {})
 
     filenames = payload.get('filename', [])
 
-    print(f"Received filenames: {filenames}")
+    # Define bucket names and table name
+    pending_images_bucket = 'retroideal-member-vehicle-images'
+    approved_images_bucket = 'retroideal-member-vehicle-images'
+    vehicle_image_table = 'retroideal-vehicle-image-table'
 
+    # Compare images in pending and approved folders
     for filename in filenames:
-        # Construct the DynamoDB key based on the primary key attribute
-        key_attribute_name = 'image-id'
-        key_value = filename
+        pending_image_key = f"pending-vehicle-images/{filename}.jpg"
+        approved_image_keys = get_all_approved_image_keys(approved_images_bucket)
 
-        # Update DynamoDB DeleteItem operation to use the correct key
-        try:
-            response = dynamodb.delete_item(
-                TableName=table_name,
-                Key={key_attribute_name: {'S': key_value}}
-            )
-            print(f"DynamoDB Delete Response for filename {filename}: {response}")
+        matching = False
+        pending_hash = get_image_hash(pending_images_bucket, pending_image_key)
 
-        except Exception as e:
-            print(f"Error deleting item for filename {filename}: {e}")
+        for approved_key in approved_image_keys:
+            approved_hash = get_image_hash(approved_images_bucket, approved_key)
+            if pending_hash - approved_hash < 5:  # Adjust threshold as needed
+                matching = True
+                break
 
-    print(f"Items deleted for the provided filenames: {filenames}")
+        # Update vehicle image table based on matching
+        update_status_in_vehicle_image_table(vehicle_image_table, filename, matching)
+
     return {
-        "statusCode": 200,
-        "body": json.dumps({"message": f"Items deleted for the provided filenames: {filenames}"})
+        'statusCode': 200,
+        'body': json.dumps('Images compared and vehicle image table updated.')
     }
+
+def get_image_hash(bucket_name, image_key):
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket=bucket_name, Key=image_key)
+    image = Image.open(response['Body'])
+    return imagehash.phash(image)
+
+def get_all_approved_image_keys(bucket_name):
+    s3 = boto3.client('s3')
+    objects = s3.list_objects_v2(Bucket=bucket_name, Prefix='approved-vehicle-images/')
+    approved_keys = [obj['Key'] for obj in objects.get('Contents', [])]
+    return approved_keys
+
+def compare_images(bucket1, key1, bucket2, key2):
+    s3 = boto3.client('s3')
+    image1 = s3.get_object(Bucket=bucket1, Key=key1)['Body'].read()
+    image2 = s3.get_object(Bucket=bucket2, Key=key2)['Body'].read()
+
+    # Compare image hashes using hashlib
+    hash1 = hashlib.sha256(image1).hexdigest()
+    hash2 = hashlib.sha256(image2).hexdigest()
+
+    return hash1 == hash2
+
+def update_status_in_vehicle_image_table(table_name, filename, matching):
+    dynamodb = boto3.client('dynamodb')
+
+    # Define the status based on matching
+    status = 'declined' if matching else 'preapproved'
+
+    # Update the status in the table
+    try:
+        response = dynamodb.update_item(
+            TableName=table_name,
+            Key={'filename': {'S': filename}},
+            UpdateExpression='SET #s = :status',
+            ExpressionAttributeNames={'#s': 'status'},
+            ExpressionAttributeValues={':status': {'S': status}}
+        )
+        print(f"Status updated for filename: {filename} -> {status}")
+    except Exception as e:
+        print(f"Error updating status for filename {filename}: {e}")
+
+    return
+
